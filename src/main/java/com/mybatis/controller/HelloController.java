d5adbcae017c13b8fef9a4e13f819cba453bd54e
516677379 <516677379@qq.com>
2017-12-17 20:36:28 +0800

    thymeleaf模板跳转及页面接收值与循环

--- src/main/java/com/mybatis/controller/HelloController.java
+++ src/main/java/com/mybatis/controller/HelloController.java
@@ -4,10 +4,11 @@ import com.alibaba.fastjson.JSONObject;
 import com.mybatis.modles.HobbyGroup;
 import com.mybatis.service.IService.HobbyIService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -15,7 +16,7 @@ import java.util.List;
 /**
  * Created by 51667 on 2017/12/15.
  */
-@RestController
+@Controller
 @RequestMapping("/hello")
 public class HelloController {
 
@@ -23,15 +24,25 @@ public class HelloController {
     private HobbyIService hobbyIService;
 
     @RequestMapping("/index")
+    @ResponseBody
     public String index(){
         List<HobbyGroup> hobbyGroupList=hobbyIService.findHobbyGroup();
         return JSONObject.toJSONString(hobbyGroupList);
     }
 
     @RequestMapping("/index2")
-    public String index2(@RequestParam(value = "hobbyGroupList",required = false)List<HobbyGroup> hobbyGroupList, Model model, ModelAndView modelAndView){
+    public ModelAndView index2(@RequestParam(value = "hobbyGroupList",required = false)List<HobbyGroup> hobbyGroupList,Model model){
         hobbyGroupList=hobbyIService.findHobbyGroup();
-        model.addAttribute("name","zhao");
-        return "hello";//返回页面
+        model.addAttribute("sex","男");
+        ModelAndView mv=new ModelAndView();
+        mv.addObject("name","tong");
+        mv.addObject("hobbyGroupList",hobbyGroupList);
+        mv.setViewName("hello");
+        return mv;//返回页面
+    }
+    @RequestMapping("/index3")
+    public String index2( Model model){
+        model.addAttribute("sex","男");
+        return "/view/test";//返回页面
     }
 }
