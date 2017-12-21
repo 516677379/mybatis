54afea924c942d3c19b40aed19c479d1310b5d12
516677379 <516677379@qq.com>
2017-12-21 19:18:03 +0800

    1.添加druid数据监控 2.pagehelper配置类提出

---
+++ src/main/java/com/mybatis/config/PageHelperCongifuration.java
@@ -0,0 +1,26 @@
+package com.mybatis.config;
+
+import com.github.pagehelper.PageHelper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+/**
+ * Created by 51667 on 2017/12/21.
+ */
+@Configuration
+public class PageHelperCongifuration {
+    //配置mybatis的分页插件pageHelper
+    @Bean
+    public PageHelper pageHelper(){
+        PageHelper pageHelper = new PageHelper();
+        Properties properties = new Properties();
+        properties.setProperty("offsetAsPageNum","true");
+        properties.setProperty("rowBoundsWithCount","true");
+        properties.setProperty("reasonable","true");
+        properties.setProperty("dialect","mysql");    //配置mysql数据库的方言
+        pageHelper.setProperties(properties);
+        return pageHelper;
+    }
+}
